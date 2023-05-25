cf80f7245bbc4e91c02659fc7a626c660e1cd575
Jose Flores <jop0693@gmail.com>
2023-05-25 09:43:24 -0600

    testInfixExpression helper

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -266,15 +266,8 @@ func TestParsingInflixExpressions(t *testing.T) {
 			t.Fatalf("exp is not ast.InfixExpression. got=%T", stmt.Expression)
 		}
 
-		if !testIntegerLiteral(t, exp.Left, tt.leftValue) {
-			return
-		}
-
-		if exp.Operator != tt.operator {
-			t.Fatalf("exp.Operator is not '%s'. got=%s", tt.operator, exp.Operator)
-		}
-
-		if !testIntegerLiteral(t, exp.Right, tt.rightValue) {
+		infx := testInfixExpression(t, exp, exp.Left, exp.Operator, exp.Right)
+		if !infx {
 			return
 		}
 	}
@@ -286,18 +279,18 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 		expected string
 	}{
 		{"-a * b", "((-a) * b)"},
-		// {"a + b", "(a + b)"},
-		// {"!-a", "(!(-a))"},
-		// {"a + b + c", "((a + b) + c)"},
-		// {"a + b - c", "((a + b) - c)"},
-		// {"a * b * c", "((a * b) * c)"},
-		// {"a * b / c", "((a * b) / c)"},
-		// {"a + b / c", "(a + (b / c))"},
-		// {"a + b * c + d / e - f", "(((a + (b * c)) + (d / e)) - f)"},
-		// {"3 + 4; -5 * 5", "(3 + 4)((-5) * 5)"},
-		// {"5 > 4 == 3 < 4", "((5 > 4) == (3 < 4))"},
-		// {"5 < 4 != 3 > 4", "((5 < 4) != (3 > 4))"},
-		// {"3 + 4 * 5 == 3 * 1 + 4 * 5", "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))"},
+		{"a + b", "(a + b)"},
+		{"!-a", "(!(-a))"},
+		{"a + b + c", "((a + b) + c)"},
+		{"a + b - c", "((a + b) - c)"},
+		{"a * b * c", "((a * b) * c)"},
+		{"a * b / c", "((a * b) / c)"},
+		{"a + b / c", "(a + (b / c))"},
+		{"a + b * c + d / e - f", "(((a + (b * c)) + (d / e)) - f)"},
+		{"3 + 4; -5 * 5", "(3 + 4)((-5) * 5)"},
+		{"5 > 4 == 3 < 4", "((5 > 4) == (3 < 4))"},
+		{"5 < 4 != 3 > 4", "((5 < 4) != (3 > 4))"},
+		{"3 + 4 * 5 == 3 * 1 + 4 * 5", "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))"},
 	}
 
 	for _, tt := range tests {
@@ -312,4 +305,61 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
 			t.Errorf("expected=%q, got=%q", tt.expected, actual)
 		}
 	}
+}
+
+func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+	ident, ok := exp.(*ast.Identifier)
+	if !ok {
+		t.Errorf("exp not *ast.Idenfifier. got=%T", exp)
+		return false
+	}
+
+	if ident.Value != value {
+		t.Errorf("ident.Value not %s. got=%s", value, ident.Value)
+		return false
+	}
+
+	if ident.TokenLiteral() != value {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", value, ident.TokenLiteral())
+		return false
+	}
+
+	return true
+}
+
+func testLiteralExpression(t *testing.T, exp ast.Expression, expected interface{}) bool {
+	switch v := expected.(type) {
+	case int:
+		return testIntegerLiteral(t, exp, int64(v))
+	case int64:
+		return testIntegerLiteral(t, exp, v)
+	case string:
+		return testIdentifier(t, exp, v)
+	}
+
+	t.Errorf("type of exp not handled. got=%T", exp)
+	return false
+}
+
+func testInfixExpression(t *testing.T, exp ast.Expression, left interface{}, operator string, right interface{}) bool {
+	opExp, ok := exp.(*ast.InfixExpression)
+	if !ok {
+		t.Errorf("exp is not ast.InfixExpression, got=%T(%s)", exp, exp)
+		return false
+	}
+
+	if !testLiteralExpression(t, opExp.Left, left) {
+		return false
+	}
+
+	if opExp.Operator != operator {
+		t.Errorf("exp.Operator is not '%s'. got=%q", operator, opExp.Operator)
+		return false
+	}
+
+	if !testLiteralExpression(t, opExp.Right, right) {
+		return false
+	}
+
+	return true
 }
